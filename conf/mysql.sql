d40227efe7389ec3f8c0ab26071e4ebf5b516d11
darkzbaron <darkzbaron@hotmail.com>
2017-02-01 14:44:31 +0100

    updated sql query and renamed config sample

--- conf/mysql.sql
+++ conf/mysql.sql
@@ -1,16 +1,16 @@
-CREATE TABLE IF NOT EXISTS contact (
-  id MEDIUMINT NOT NULL AUTO_INCREMENT,
-  firstname VARCHAR(15) DEFAULT NULL,
-  lastname VARCHAR(15) DEFAULT NULL,
-  PRIMARY KEY   (id),
-  KEY firstname (firstname),
-  KEY lastname  (lastname)
+CREATE TABLE IF NOT EXISTS contactinfo (
+contact_id MEDIUMINT NOT NULL,
+type ENUM('homephone','workphone','mobilephone','otherphone','email', 'other') DEFAULT 'homephone',
+info VARCHAR(20) DEFAULT NULL,
+PRIMARY KEY (contact_id, type, info),
+FOREIGN KEY (contact_id) REFERENCES contact(id) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=INNODB DEFAULT CHARSET=latin1;
 
-CREATE TABLE IF NOT EXISTS contactinfo (
-  contact_id MEDIUMINT NOT NULL,
-  type ENUM('homephone','workphone','mobilephone','otherphone','email', other) DEFAULT 'home',
-  info VARCHAR(20) DEFAULT NULL,
-  PRIMARY KEY (contact_id, type, info),
-  FOREIGN KEY (contact_id) REFERENCES contact(id) ON DELETE CASCADE ON UPDATE CASCADE
+CREATE TABLE IF NOT EXISTS contact (
+id MEDIUMINT NOT NULL AUTO_INCREMENT,
+firstname VARCHAR(15) DEFAULT NULL,
+lastname VARCHAR(15) DEFAULT NULL,
+PRIMARY KEY (id),
+KEY firstname (firstname),
+KEY lastname (lastname)
 ) ENGINE=INNODB DEFAULT CHARSET=latin1;
